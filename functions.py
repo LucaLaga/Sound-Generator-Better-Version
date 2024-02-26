97d0a0b263b7dff44af42263aaefc6342697ab83
LucaLaga <lucalagana998@gmail.com>
2024-02-26 19:57:56 +0100

    initial commit

--- functions.py
+++ functions.py
@@ -3,6 +3,20 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
+def get_noteFrequency(name, octave):
+    note = ['C', 'C#', 'D', 'Eb', 'E', 'F', 'F#', 'G', 'G#', 'A', 'Bb', 'B']
+    frequencies = [[16.35, 17.32, 18.35, 19.45, 20.6, 21.83, 23.12, 24.50, 25.96, 27.50, 29.14, 30.87],
+                   [32.7, 34.65, 36.71, 38.89, 41.2, 43.65, 46.25, 49, 51.91, 55, 58.27, 61.74],
+                   [65.41, 69.3, 73.42, 77.78, 82.41, 87.31, 92.5, 98, 103.8, 110, 116.5, 123.5],
+                   [130.8, 138.6, 146.8, 155.6, 164.8, 174.6, 185, 196, 207.7, 220, 233.1, 246.9],
+                   [261.6, 277.2, 293.7, 311.1, 329.6, 349.2, 370, 392, 415, 440, 466.2, 493.9],
+                   [523.3, 554.4, 587.3, 622.3, 659.3, 698.5, 740, 784, 830.6, 880, 932.3, 987.8],
+                   [1047, 1109, 1175, 1245, 1319, 1397, 1480, 1568, 1661, 1760, 1865, 1976],
+                   [2093, 2217, 2349, 2489, 2637, 2794, 2960, 3136, 3322, 3520, 3729, 3951],
+                   [4186, 4435, 4699, 4978,5274, 5588, 5920, 6272, 6645, 7040, 7459, 7902]]
+    
+    return frequencies[octave][note.index(name)]
+
 def generateHarmonicsSounds(rootFrequency, duration, samplingRate):
     frequencies = []
     for i in range(1, 17):
@@ -35,9 +49,25 @@ def generateHarmonicsSounds(rootFrequency, duration, samplingRate):
 
     return audioData
 
+def generate_chordFreqs(rootFreq, chordType=1):
+    match chordType:
+        case 1:
+            return np.array([rootFreq, (5 / 4) * rootFreq, (3 / 2) * rootFreq])
+        case 2:
+            return np.array([rootFreq, (6 / 5) * rootFreq, (3 / 2) * rootFreq])
+        case 3:
+            return np.array([rootFreq, (6 /5) * rootFreq, (45 / 32) * rootFreq])
+        case 4:
+            return np.array([rootFreq, (9 / 8) * rootFreq, (3 / 2) * rootFreq])
+        case 5:
+            return np.array([rootFreq, (4 / 3) * rootFreq, (3 / 2) * rootFreq])
+        case 6:
+            return np.array([rootFreq, (5 / 4) * rootFreq, (8 / 5) * rootFreq])
+        case _:
+            return np.array([rootFreq, (5 / 4) * rootFreq, (3 / 2) * rootFreq])
 
-def generateMajorChord(rootFrequency, duration, samplingRate):
-    frequencies = [rootFrequency, rootFrequency / (4 / 5), rootFrequency / (2 / 3)]
+def generateChord(rootFrequency, duration, samplingRate, chordType=1):
+    frequencies = generate_chordFreqs(rootFrequency, chordType)
 
     t = np.linspace(0, duration, int(samplingRate * duration), endpoint=False)
     audioData = np.zeros(len(t))
@@ -92,24 +122,18 @@ def playNoteByHarmonics(rootFrequency, duration, samplingRate):
     pygame.time.wait(int(10 * 1000))
 
 
-def playMajorChordByHarmonicsRatio(rootFrequency, duration, samplingRate):
-    majorChord = generateMajorChord(rootFrequency, duration, samplingRate)
-    majorChord = np.column_stack((majorChord, majorChord))
-    majorChord = np.ascontiguousarray(majorChord)
-    majorChordSound = pygame.sndarray.make_sound(majorChord)
-    majorChordSound.play()
+def playChordByHarmonicsRatio(rootFrequency, duration, samplingRate, chordType=1):
+    chord = generateChord(rootFrequency, duration, samplingRate, chordType)
+    chord = np.column_stack((chord, chord))
+    chord = np.ascontiguousarray(chord)
+    chordSound = pygame.sndarray.make_sound(chord)
+    chordSound.play()
     pygame.time.wait(int(duration * 1000))
 
 
-def playMajorChordSingleNotes(rootFrequency, duration, samplingRate):
-    for i in range(3):
-        if i == 0:
-            noteFrequency = rootFrequency
-        elif i == 1:
-            noteFrequency = rootFrequency / (4 / 5)
-        else:
-            noteFrequency = rootFrequency * (3 / 2)
-
+def playChordSingleNotes(rootFrequency, duration, samplingRate, chordType=1):
+    freqs = generate_chordFreqs(rootFrequency, chordType)
+    for noteFrequency in freqs:
         noteAudioData = generateSingleNote(noteFrequency, duration, samplingRate)
         noteAudioData = np.column_stack((noteAudioData, noteAudioData))
         noteAudioData = np.ascontiguousarray(noteAudioData)
--- main.py
+++ main.py
@@ -9,11 +9,36 @@ duration = 5.0
 pygame.init()
 pygame.mixer.init(samplingRate, -16, 2)
 
-rootFrequency = float(input("Inserisci il valore in hz della frequenza di base: "))
+choice = int(input("1. Inserire la frequenza\n2. Inserire il nome della nota\n-->"))
+
+match choice:
+    case 1:
+        rootFrequency = float(input("Inserisci il valore in hz della frequenza di base: "))
+    case 2:
+        noteName = input("Inserisci il nome della nota\nC\tC#\tD\tEb\tE\tF\tF#\tG\tG#\tA\tBb\tB\n-->")
+        octave = int(input("Inserisci l'ottava (Da 0 a 8): "))
+        rootFrequency = get_noteFrequency(noteName, octave)
+    case _:
+        rootFrequency = float(input("Inserisci il valore in hz della frequenza di base: "))
+
+"""
+1. Major
+2. Minor
+3. Diminished
+4. Sus2
+5. Sus4
+6. Augmented
+None => Major
+"""
+
+chordType = input("Inserisci il tipo di accordo (da 1 a 6 oppure vuto)\n-->")
+
+if chordType != None:
+    chordType = int(chordType)
 
 playNoteByHarmonics(rootFrequency, duration, samplingRate)
-playMajorChordByHarmonicsRatio(rootFrequency, duration, samplingRate)
-playMajorChordSingleNotes(rootFrequency, duration, samplingRate)
+playChordByHarmonicsRatio(rootFrequency, duration, samplingRate, chordType)
+playChordSingleNotes(rootFrequency, duration, samplingRate, chordType)
 plt.show()
 
 pygame.quit()
